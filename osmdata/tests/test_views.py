ac0d4f0f1ec33c5cc9207f57ed776c90205af6d5
Jocelyn Delalande <jocelyn@crapouillou.net>
2017-04-24 20:42:22 +0200

    Fix typo in filter name
    Fix #5

--- osmdata/tests/test_views.py
+++ osmdata/tests/test_views.py
@@ -9,3 +9,8 @@ class ViewsTest(TestCase):
         # exporters
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<osm', response.content)
+        # test some (quite random) strings that are supposed to be present in
+        # output, as it seems that a HTTP 200 is returned, even in case of
+        # TemplateSyntaxError.
+        self.assertIn(b'visible="true"', response.content)
+        self.assertIn(b'fr:Gare de Versailles-Chantiers', response.content)
